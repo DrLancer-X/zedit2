26236c1d7804630db50de4f277564f32c9f36c89
Dr Lancer-X <drlancer@megazeux.org>
2024-02-19 19:25:30 +1000

    Updated help file and version number to reflect the last two commits

--- src/zedit2/Main.java
+++ src/zedit2/Main.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import javax.swing.*;
 
 public class Main implements Runnable {
-    public static final String VERSION = "0.38";
+    public static final String VERSION = "0.39";
     private static String[] args;
     public static void main(String[] args) {
         //System.setProperty("sun.java2d.opengl", "True");
